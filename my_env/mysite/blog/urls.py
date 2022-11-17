f77b874b94f5d38179eee70da4f817f0ba206ac0
TULUS <tulus@localhost.localdomain>
2022-11-17 11:04:38 +0300

    Changes

--- my_env/mysite/blog/urls.py
+++ my_env/mysite/blog/urls.py
@@ -4,8 +4,10 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # post views
+    # path('', views.post_list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-    views.post_detail,
-    name='post_detail'),
+        views.post_detail,
+        name='post_detail'),
 ]
